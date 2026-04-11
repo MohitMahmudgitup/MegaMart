4a74f34aa31a83ec2aa7c32b1785e21ad478891d
MohitMahmudgitup <syedmohitmahmudinzmaqm@gmail.com>
2026-04-11 11:54:55 +0600

    feat: add Facebook Pixel tracking script to RootLayout component

--- mega-mart-base-customer/src/app/layout.tsx
+++ mega-mart-base-customer/src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "react-hot-toast";
 import "./globals.css";
 import { AuthProvider } from "@/provider/AuthProvider";
 import LenisProvider from "@/components/LenisProvider";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,7 @@ export async function generateMetadata(): Promise<Metadata> {
 
     const json = await res.json();
 
-    const site = json?.data?.[0]; 
+    const site = json?.data?.[0];
 
     return {
       title: site?.siteName || "CartX",
@@ -53,6 +54,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;
+    n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];
+    t=b.createElement(e);t.async=!0;
+    t.src=v;
+    s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)
+    }(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '1653690892311826');
+    fbq('track', 'PageView');
+  `}
+      </Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
